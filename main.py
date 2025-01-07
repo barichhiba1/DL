a2c24ae790a96baa3ffb09e3b16105a5e1cdee89
hiba <barichhiba381@gmail.com>
2025-01-07 21:50:53 +0100

    second  commit

--- main.py
+++ main.py
@@ -1,59 +1,46 @@
-from flask import Flask, request, render_template, redirect, url_for
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from PIL import Image
-import numpy as np
-import os
-
-# Initialiser l'application Flask
-app = Flask(__name__)
-
-# Charger le modèle
-model = load_model("model/CNN_fruits.h5")
-
-# Classes cibles
-class_names = ['apple', 'banana', 'orange']
 
 
-# Fonction de prédiction
+#st.write("write testest")
+
+#charger le modele
+model=load_model("model/CNN_fruits.h5")
+
+#les classes
+class_names=['apple','banana','orange']
+
+#fonction de prediction
+#tout ce qu'on a fait dans le modele doit etre fait ici
 def predict(image):
-    image = image.convert('RGB')  # Convertir en RGB
-    img = image.resize((32, 32))  # Redimensionner l'image
-    img_array = img_to_array(img) / 255.0  # Normaliser
-    img_array = np.expand_dims(img_array, axis=0)  # Ajouter une dimension
-    predictions = model.predict(img_array)
-    class_index = np.argmax(predictions)
-    confidence = np.max(predictions)
-    return class_names[class_index], confidence
+     # Convertir l'image en RGB (au cas où elle serait RGBA ou en mode autre que RGB)
+    image = image.convert('RGB')
+    img=image.resize((32,32))
+    img_array=img_to_array(img)/255.0
+    img_array=np.expand_dims(img_array,axis=0)
+    predictions=model.predict(img_array)
+    class_index=np.argmax(predictions)
+    confidence=np.max(predictions)
+    return class_names[class_index],confidence
 
 
-# Route pour la page principale
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        # Vérifier si un fichier a été uploadé
-        if "file" not in request.files:
-            return redirect(request.url)
+#interface streamlit
+st.title("CNN modele pour classification des fruits")
+st.write("Ce modele va predire si le fruit est une banane , pomme ou orange :")
 
-        file = request.files["file"]
-        if file.filename == "":
-            return redirect(request.url)
+#charger l'image
+uploaded_file=st.file_uploader("Telecharger l'image", type=['png','jpg','jpeg'])
 
-        if file:
-            # Charger l'image
-            image = Image.open(file)
-            # Prédire la classe
-            class_name, confidence = predict(image)
-            return render_template(
-                "index.html",
-                class_name=class_name,
-                confidence=f"{confidence * 100:.2f}%",
-                uploaded_image=file.filename
-            )
-
-    return render_template("index.html")
-
-
-# Lancer l'application
-if __name__ == "__main__":
-    app.run(debug=True)
+if uploaded_file:
+    image=Image.open(uploaded_file)
+    st.image(image,caption="Image telechargé", use_container_width=True) #utilise la taille du conteneur
+    #Prediction
+    #spinner : affiche un message en attendant la fin de l'analyse
+    with st.spinner("Analyse en cours"):
+        class_name,confidence=predict(image)
+        st.success(f"Résultat : {class_name}({confidence*100:.2f}%)")
